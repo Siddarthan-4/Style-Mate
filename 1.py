53d6842ae4627eb76dac65123f9239c17107c0de
myname <me@example.com>
2025-10-26 10:25:26 +0530

    updated project

--- 1.py
+++ 1.py
@@ -4,8 +4,10 @@ import os
 
 proj = Flask(__name__)
 
-folder="imgs"
-os.makedirs(folder, exist_ok=True)
+f1="uimgs"
+f2="limgs"
+os.makedirs(f1, exist_ok=True)
+os.makedirs(f2, exist_ok=True)
 
 @proj.route("/")
 def home():
@@ -14,22 +16,40 @@ def home():
 
 @proj.route("/analyse",methods=["POST"])
 def upload():
-    results=[]
+    results1=[]
+    results2=[]
 
-    images=request.files.getlist("file")
-    for img in images:
-        img_path=os.path.join(folder,img.filename)
-        img.save(img_path)
+    upper_images=request.files.getlist("uo")
+    lower_images=request.files.getlist("lo")
 
-        col=ColorThief(img_path)
+    for img in upper_images:
+        img_path1=os.path.join(f1,img.filename)
+        img.save(img_path1)
+
+        col=ColorThief(img_path1)
         domcol=col.get_color(quality=1)
 
-        results.append({
+        results1.append({
             "filename":img.filename,
             "dominant_color":domcol
         })
 
-    return jsonify({"files":results})
+    for img in lower_images:
+        img_path2=os.path.join(f2,img.filename)
+        img.save(img_path2)
 
+        col=ColorThief(img_path2)
+        domcol=col.get_color(quality=1)
+
+        results2.append({
+            "filename":img.filename,
+            "dominant_color":domcol
+        })
+
+        return jsonify({
+                        "upper images":results1,
+                        "lower images":results2
+                        }) 
+    
 if __name__ == '__main__':
     proj.run(debug=True)
--- file_upload.tsx
+++ file_upload.tsx
@@ -1,19 +1,31 @@
 import React from "react"
-import "./form/file_upload.css"
+import "./python_mini_proj/file_upload.css"
+import header from "./python_mini_proj/header"
 
 export default function fun(){
 
   return(
     <>
-    <form action="http://127.0.0.1:5000/analyse" method="post" encType="multipart/form-data"
->
-        <fieldset>
-        <legend>FILE UPLOAD</legend>
-        <label>Upload your files : </label>
-        <input type="file" name="file" multiple></input> 
+    <div>
+        <img src="public/clothes.png" alt="icon"/>
+        <h1>Style Mate</h1>
+    </div>
+    <br/>
+    <form action="http://127.0.0.1:5000/analyse" method="post" encType="multipart/form-data">
+       
+        <label>Upload images of your shirts/T-shirts(upper outfits) : </label>
+        <br/>
+        <input type="file" name="uo" multiple></input> 
+        <br/>
+        <label>Upload images of your pants/Track pants(lower outfits) : </label>
+        <br/>
+        <input type="file" name="lo" multiple></input> 
+        <br/>
+        <br/>
         <button type="submit">Upload</button>
-        </fieldset>
+       
     </form>
+
     </>
   )  
 }
